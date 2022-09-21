7b41b5eddc50355dc3efffafad461aed7ecb2f38
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2022-09-21 16:36:22 -0300

    ajusted image positioning

--- src/components/Cards/PokemonCards.jsx
+++ src/components/Cards/PokemonCards.jsx
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from 'react'
-import { Card, Row, Col } from 'antd';
+import { Card, Row, Col, Space } from 'antd';
 
 const { Meta } = Card;
 
@@ -7,7 +7,7 @@ function PokemonCards() {
 	const [data, setData] = useState([]);
 
 	useEffect(() => {
-		fetch("https://pokeapi.co/api/v2/pokemon?limit=9")
+		fetch("https://pokeapi.co/api/v2/pokemon?limit=1")
 			.then((res) => res.json())
 			.then((apiData) => {
 				const { results } = apiData;
@@ -16,28 +16,33 @@ function PokemonCards() {
 			})
 	}, []);
 
-	// function FetchImage(object) {
-	// 	fetch(object.url)
-	// 		.then((res) => res.json())
-	// 		.then((pokemonData) => {
-	// 			console.log(JSON.stringify(pokemonData.sprites.other["official-artwork"].front_default))
-	// 			return JSON.stringify(pokemonData.sprites.other["official-artwork"].front_default);
-	// 		})
-	// }
+	function GetPokemonType(url) {
+		fetch(url)
+			.then((res) => res.json())
+			.then((pokemonData) => {
+				console.log(pokemonData.types) // retorna uma lista
+			})
+	}
 
 	return (
-		<Row className='cards-pokemon'>
+		<Row
+			className='cards-pokemon-row'
+		>
 			{data.map((pokemon, idx) => (
 				<Col
+					className='card-col'
 					key={idx}
 					span={8}
 				>
 					<Card
+						className='pokemon-card'
 						hoverable
 						cover={<img alt="pokemon-image" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${idx + 1}.png`} />}
-						style={{ width: 280 }}
 					>
-						<Meta title={pokemon.name} />
+						<Meta
+							title={pokemon.name}
+							description={GetPokemonType(`https://pokeapi.co/api/v2/pokemon/${idx + 1}`)}
+						/>
 					</Card>
 				</Col>
 			))
