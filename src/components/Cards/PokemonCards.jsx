0f809c6c0543e569b7b16f6048141a1e2c6db187
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2022-11-25 17:16:05 -0300

    Added types logic and display via collapse from ANTD

--- src/components/Cards/PokemonCards.jsx
+++ src/components/Cards/PokemonCards.jsx
@@ -1,9 +1,15 @@
 import { React } from 'react'
-import { Card, Row, Col, Space } from 'antd';
+import { Card, Row, Col, Collapse  } from 'antd';
 
+const { Panel } = Collapse;
 const { Meta } = Card;
 
 function PokemonCards({ pokemons }) {
+
+	const toCapitalLetter = (name) => {
+		return name.charAt(0).toUpperCase() + name.slice(1)
+	}
+
 	return (
 		<Row
 			className='cards-pokemon-row'
@@ -20,8 +26,22 @@ function PokemonCards({ pokemons }) {
 						cover={<img alt="pokemon-image" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${idx + 1}.png`} />}
 					>
 						<Meta
-							title={pokemon.name}
-							description={"teste"}
+							className='Card-meta-class'
+							title={toCapitalLetter(pokemon.name)}
+							description={
+							<Collapse
+								key={idx}
+								bordered={false}
+								ghost={false}
+							>
+								<Panel header={"Types"}>
+									{pokemon.types.map((type, idx) => {
+										<p key={idx}>
+											{toCapitalLetter(type.type.name)}
+										</p>
+									})}
+								</Panel>
+							</Collapse>}
 						/>
 					</Card>
 				</Col>
