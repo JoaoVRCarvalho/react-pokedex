f1a2b086912e41e2e1fd88a1a0b9fa4c051ee6a3
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2022-12-12 14:43:07 -0300

    Created a separete component for cards

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Card, Button } from 'antd';
+
+const { Meta } = Card;
+
+function Cards({ pokemon, handleModal }) {
+
+	const toCapitalLetter = name => {
+		return name.charAt(0).toUpperCase() + name.slice(1)
+	}
+
+	return (
+		<Card
+			className='pokemon-card'
+			hoverable
+			cover={
+				<img
+					alt="pokemon"
+					src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+				/>
+			}
+		>
+			<Meta
+				className='Card-meta-class'
+				title={toCapitalLetter(pokemon.name)}
+				description={
+					<>
+						<div className='types-wrapper'>
+							{pokemon.types.map((type, index) => (
+								<span
+									key={index}
+									className={`pokemon-type-${type.type.name} pokemon-type`}
+								>
+									{toCapitalLetter(type.type.name)}
+								</span>
+							))}
+						</div>
+						<Button
+							className='pokemon-details-button'
+							type="primary"
+							danger
+							onClick={() => handleModal(pokemon)}
+						>
+							Details!
+						</Button>
+					</>
+				}
+			/>
+		</Card>
+	)
+}
+
+export default Cards;
